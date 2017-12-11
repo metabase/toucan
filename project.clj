5ccec157cbe5ae30be520dca12f54e00c445f9f3
Cam Saul <cammsaul@gmail.com>
2017-12-11 15:56:11 -0800

    Update dependencies & bump version -> 1.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,25 +1,25 @@
-(defproject toucan "1.1.1"
+(defproject toucan "1.1.2-SNAPSHOT"
   :description "Functionality for defining your application's models and querying the database."
   :url "https://github.com/metabase/toucan"
   :license {:name "Eclipse Public License"
             :url "https://raw.githubusercontent.com/metabase/toucan/master/LICENSE.txt"}
   :min-lein-version "2.5.0"
   :dependencies [[org.clojure/java.classpath "0.2.3"]
-                 [org.clojure/java.jdbc "0.6.1"]
+                 [org.clojure/java.jdbc "0.7.3"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.clojure/tools.namespace "0.2.10"]
-                 [honeysql "0.8.2"]]
+                 [honeysql "0.9.1"]]
   :javac-options ["-target" "1.7", "-source" "1.7"]
-  :aliases {"bikeshed" ["bikeshed" "--max-line-length" "130"]
+  :aliases {"bikeshed" ["bikeshed" "--max-line-length" "130" "--var-redefs" "false"]
             "lint" ["do" ["eastwood"] "bikeshed" "docstring-checker"]
             "test" ["expectations"]}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [expectations "2.1.9"]
                                   [postgresql "9.3-1102.jdbc41"]]
-                   :plugins [[docstring-checker "1.0.0"]
-                             [jonase/eastwood "0.2.4"
+                   :plugins [[docstring-checker "1.0.2"]
+                             [jonase/eastwood "0.2.5"
                               :exclusions [org.clojure/clojure]]
-                             [lein-bikeshed "0.4.1"]
+                             [lein-bikeshed "0.5.0"]
                              [lein-expectations "0.0.8"]]
                    :jvm-opts ["-Xverify:none"]
                    :eastwood {:add-linters [:unused-locals
