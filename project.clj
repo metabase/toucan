d628a10977a7f3157cf1abcd057abfe5ab67e380
Cam Saul <cammsaul@gmail.com>
2017-12-29 15:22:12 -0800

    Bump JDBC dep version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://raw.githubusercontent.com/metabase/toucan/master/LICENSE.txt"}
   :min-lein-version "2.5.0"
   :dependencies [[org.clojure/java.classpath "0.2.3"]
-                 [org.clojure/java.jdbc "0.7.4"]
+                 [org.clojure/java.jdbc "0.7.5"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.clojure/tools.namespace "0.2.10"]
                  [honeysql "0.9.1"]]
