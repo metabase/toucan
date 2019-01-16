c024f2da8d89431ddf798ee7b13002f5076854bd
Cam Saul <cammsaul@gmail.com>
2019-01-15 17:08:28 -0800

    Bump dependencies :cool:

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "https://raw.githubusercontent.com/metabase/toucan/master/LICENSE.txt"}
   :min-lein-version "2.5.0"
   :dependencies [[org.clojure/java.classpath "0.3.0"]
-                 [org.clojure/java.jdbc "0.7.7"]
+                 [org.clojure/java.jdbc "0.7.8"]
                  [org.clojure/tools.logging "0.4.1"]
                  [org.clojure/tools.namespace "0.2.10"]
-                 [honeysql "0.9.3"]]
+                 [honeysql "0.9.4"]]
   :javac-options ["-target" "1.7", "-source" "1.7"]
   :aliases {"bikeshed" ["bikeshed" "--max-line-length" "118" "--var-redefs" "false"]
             "lint" ["do" ["eastwood"] "bikeshed" "docstring-checker" "check-namespace-decls"]
@@ -16,15 +16,14 @@
             "start-db" ["shell" "./start-db"] ; `lein start-db` and stop-db are conveniences for running a test database via Docker
             "stop-db" ["shell" "docker" "stop" "toucan_test"]}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
-                                  [expectations "2.1.9"]
+                                  [expectations "2.1.10"]
                                   [postgresql "9.3-1102.jdbc41"]]
-                   :plugins [[docstring-checker "1.0.2"]
-                             [jonase/eastwood "0.2.8"
+                   :plugins [[docstring-checker "1.0.3"]
+                             [jonase/eastwood "0.3.4"
                               :exclusions [org.clojure/clojure]]
                              [lein-bikeshed "0.5.1"]
                              [lein-check-namespace-decls "1.0.1"]
-                             [lein-expectations "0.0.8"]
-                             [org.clojure/tools.cli "0.3.7"]] ; Added because of https://github.com/dakrone/lein-bikeshed/issues/33
+                             [lein-expectations "0.0.8"]]
                    :jvm-opts ["-Xverify:none"]
                    :eastwood {:add-linters [:unused-locals
                                             :unused-private-vars]
