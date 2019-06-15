c6fa354d77532e9990e9bde132b4b67bc1bf4d36
Cam Saul <cammsaul@gmail.com>
2019-06-14 17:12:52 -0700

    Update Emacs .dir-locals.el
    [ci skip]

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
-((clojure-mode . ((eval . (define-clojure-indent
-                            (assert 1)
-                            (assoc 1)
-                            (ex-info 1)
-                            (match 1)
-                            (merge-with 1)
-                            (with-redefs-fn 1))))))
+((nil . ((indent-tabs-mode . nil)       ; always use spaces for tabs
+         (require-final-newline . t)))  ; add final newline on save
+ (clojure-mode . (;; if you're using clj-refactor (highly recommended!), prefer prefix notation when cleaning the ns form
+                  (cljr-favor-prefix-notation . t)
+                  ;; prefer keeping source width about ~118, GitHub seems to cut off stuff at either 119 or 120 and
+                  ;; it's nicer to look at code in GH when you don't have to scroll back and forth
+                  (fill-column . 118)
+                  (clojure-docstring-fill-column . 118))))
