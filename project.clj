e8fbd6dde8452f72ae9adb1c8b1dd3ca757e3129
Cam Saul <1455846+camsaul@users.noreply.github.com>
2020-03-23 16:55:50 -0700

    release 1.51.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject toucan "1.15.1-SNAPSHOT"
+(defproject toucan "1.15.1"
   :description "Functionality for defining your application's models and querying the database."
   :url "https://github.com/metabase/toucan"
   :license {:name "Eclipse Public License"
